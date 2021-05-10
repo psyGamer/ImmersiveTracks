04c14ed3f331088c59ea6e650a10cf0623cc434c
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-05-10 10:20:56 +0200

    Cleaned up RedstoneUtil

--- src/main/java/dev/psyGamer/immersiveTracks/util/RedstoneUtil.java
+++ src/main/java/dev/psyGamer/immersiveTracks/util/RedstoneUtil.java
@@ -1,40 +1,44 @@
 
 package dev.psyGamer.immersiveTracks.util;
 
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.init.Blocks;
+import net.minecraft.block.state.IBlockState;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class RedstoneUtil {
 	
-	public static Map<EnumFacing, Boolean> fromWhereIsBlockPowered(World world, BlockPos pos) {
-		Map<EnumFacing, Boolean> poweredSides = new HashMap() {{
-			put(EnumFacing.NORTH, false);
-			put(EnumFacing.SOUTH, false);
-			put(EnumFacing.EAST, false);
-			put(EnumFacing.WEST, false);
-		}};
+	public static Map<EnumFacing, Boolean> getPoweredSides(World world, BlockPos pos) {
+		Map<EnumFacing, Boolean> poweredSides = new HashMap<>();
 		
-		for (EnumFacing direction : poweredSides.keySet()) {
-			if (world.getRedstonePower(pos.offset(direction), direction) > 0) {
-				poweredSides.put(direction, true);
+		getPowerOnSides(world, pos).forEach((direction, power) -> poweredSides.put(direction, power > 0));
+		
+		return poweredSides;
+	}
+	
+	public static Map<EnumFacing, Integer> getPowerOnSides(World world, BlockPos pos) {
+		Map<EnumFacing, Integer> sidePower = new HashMap();
+		
+		for (EnumFacing direction : EnumFacing.values()) {
+			sidePower.put(direction, world.getRedstonePower(pos.offset(direction), direction));
+			
+			if (sidePower.get(direction) > 0 || direction == EnumFacing.UP || direction == EnumFacing.DOWN)
 				continue;
-			}
 			
-			IBlockState blockState = world.getBlockState(pos.offset(direction));
+			IBlockState blockState = world.getBlockState(pos);
+			IBlockState offsetBlockState = world.getBlockState(pos.offset(direction));
 			
-			if (	   world.getBlockState(pos).getBlock().canConnectRedstone(blockState, world, pos, direction)
-					&& blockState.getBlock() == Blocks.REDSTONE_WIRE
-					&& blockState.getValue(Blocks.REDSTONE_WIRE.POWER) > 0) {
-				poweredSides.put(direction, true);
+			if (	blockState.getBlock().canConnectRedstone(offsetBlockState, world, pos, direction)
+				 && offsetBlockState.getBlock() == Blocks.REDSTONE_WIRE) {
+				
+				sidePower.put(direction, offsetBlockState.getValue(Blocks.REDSTONE_WIRE.POWER));
 			}
 		}
 		
-		return poweredSides;
+		return sidePower;
 	}
 }
