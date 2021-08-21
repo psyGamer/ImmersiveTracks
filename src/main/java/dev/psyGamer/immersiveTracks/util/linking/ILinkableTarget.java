7a4584fc79d07797bfd459f58cf809e3b504e7a1
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-08-21 20:51:18 +0200

    Removed ILinkableSource & ILinkableTarget

--- src/main/java/dev/psyGamer/immersiveTracks/util/linking/ILinkableTarget.java
+++
@@ -1,9 +0,0 @@
-package dev.psyGamer.immersiveTracks.util.linking;
-
-import cam72cam.mod.math.Vec3i;
-import cam72cam.mod.world.World;
-
-public interface ILinkableTarget {
-	
-	boolean isValidSource(World world, Vec3i pos);
-}
