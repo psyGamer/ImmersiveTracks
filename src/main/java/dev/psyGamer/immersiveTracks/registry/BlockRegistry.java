d533d33ead087795f1eceaa122f2668eac78eeea
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-08-21 19:23:20 +0200

    Cleaned up BlockRegistry

--- src/main/java/dev/psyGamer/immersiveTracks/registry/BlockRegistry.java
+++ src/main/java/dev/psyGamer/immersiveTracks/registry/BlockRegistry.java
@@ -6,22 +6,12 @@ import dev.psyGamer.immersiveTracks.block.SignalBlock;
 import dev.psyGamer.immersiveTracks.block.SignalControllerBlock;
 import dev.psyGamer.immersiveTracks.block.SignalPoleBlock;
 import dev.psyGamer.immersiveTracks.util.Pair;
-import dev.psyGamer.immersiveTracks.util.model.AdvancedBoundingBox;
-import dev.psyGamer.immersiveTracks.util.model.IModelRegistry;
+
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Mod.EventBusSubscriber
 public class BlockRegistry {
 	
 	private static final List<Block> blocks = new ArrayList<>();
@@ -33,27 +23,4 @@ public class BlockRegistry {
 	
 	public static final BlockType SIGNAL_POLE = new SignalPoleBlock("signal_pole");
 	public static final BlockType SIGNAL_CONTROLLER = new SignalControllerBlock();
-	
-	public static void registerBlock(final Block block) {
-		BlockRegistry.blocks.add(block);
-	}
-	
-	@SubscribeEvent
-	public static void registerBlocks(final RegistryEvent.Register<Block> event) {
-		event.getRegistry().registerAll(BlockRegistry.blocks.toArray(new Block[0]));
-	}
-	
-	@SubscribeEvent
-	public static void registerBlockModels(final ModelRegistryEvent event) {
-		for (final Block block : BlockRegistry.blocks) {
-			if (block instanceof IModelRegistry) {
-				((IModelRegistry) block).registerModel();
-			}
-		}
-	}
-	
-	@SideOnly(Side.CLIENT)
-	public static void registerBlockColors() {
-		Minecraft.getMinecraft().getBlockColors().registerBlockColorHandler((state, worldIn, pos, tintIndex) -> SignalBlock.getTintColor(worldIn, pos, tintIndex - 10), BlockRegistry.SIGNAL);
-	}
 }
